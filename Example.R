03f075a10fbc582e26597854cb64a6040be646f4
wfinsinger <walter.finsinger@um2.fr>
2017-08-10 14:26:28 +0200

    R code to run ARCO with example files

---
+++ Example.R
@@ -0,0 +1,12 @@
+
+# First set the working directory...the path should point to the directory where
+# the 4 csv files and the ARCO_v1.r script are stored.
+# For instance, on my laptop it looks like this:
+setwd("/Users/wfinsing/Documents/GitHub/ARCO")
+
+# Loads the ARCO_v1.r script into the R session:
+source("ARCO_v1.r")
+
+# This command will let you run the ARCO_v1.r script using the example *.csv files
+arco(Seedle.file="Seedle.csv", Smpl.file="Smpl.csv",
+     FireA.file="FireA.csv", FireC.file="FireC.csv")
